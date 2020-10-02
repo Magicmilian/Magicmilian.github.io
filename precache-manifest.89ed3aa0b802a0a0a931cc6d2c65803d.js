f77f852565220f6d9cbfe4aa05e6b5cb1e7b6d33
Maximiliano Diaz Huerta <diazhuerta93@gmail.com>
2020-10-02 05:04:49 -0300

    Updates

---
+++ precache-manifest.89ed3aa0b802a0a0a931cc6d2c65803d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca13d4b0d2ac79e0b62da3997c558a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aee7a65b2506380ae8a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "17df624c5838c9c040bd",
+    "url": "/static/css/main.61ade7ec.chunk.css"
+  },
+  {
+    "revision": "1aee7a65b2506380ae8a",
+    "url": "/static/js/2.2093d5e7.chunk.js"
+  },
+  {
+    "revision": "6f9c91f92830cd94e0e9d23e81437a49",
+    "url": "/static/js/2.2093d5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17df624c5838c9c040bd",
+    "url": "/static/js/main.b40db6b5.chunk.js"
+  },
+  {
+    "revision": "013e568e41944fdad4ce",
+    "url": "/static/js/runtime-main.c1dc6695.js"
+  }
+]);
