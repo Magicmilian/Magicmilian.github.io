9059a83b9b48d5b572afa0f21ad752be4c13c749
Maximiliano Diaz Huerta <diazhuerta93@gmail.com>
2021-02-02 11:40:09 -0300

    Agregado proyecto Corcho Menu - Actualizadas dependencias dentro de semver range

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,11 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import "./portfolio.css";
-
 
 const Portfolio = () => {
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   const background = {
     backgroundImage: "url(/background.jpg)",
     minHeight: "calc(100vh - 80px)",
@@ -16,24 +16,114 @@ const Portfolio = () => {
     <div style={background}>
       <div className="container py-5">
         <section className="mb-4">
-          <p className="unicaFont portfolioTitle col-12">
-            Proyectos
-          </p>
+          <p className="unicaFont portfolioTitle col-12">Proyectos</p>
           <div className="projectsContainer d-flex flex-wrap justify-content-around exoFont">
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <Carousel indicators={false}>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-1.jpg"}
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-2.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-3.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-4.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-5.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-6.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-7.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
+                href="https://corcho-menu.netlify.app/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <div className="card-body">
+                  <h5 className="card-title text-center text-md-left">
+                    Corcho Menu - Menu Digital Simil Carta
+                  </h5>
+                  <p className="card-text text-justify">
+                    Proyecto realizado para Bar Tucumano con el fin de ofrecer
+                    una opcion digitalizada de la carta utilizada por el
+                    comercio. Se busco con el mismo, emular la distribucion de
+                    la informacion de modo que se asemeje a una carta
+                    tradicional. Se desarrollo frontend utilizando React.js, y
+                    backend utilizando MongoDB mediante Express.js. A nivel
+                    backend se aplicaron rutas protegidas mediante JWT,
+                    validacion de usuario mediante Google reCaptcha V2,
+                    restablecimiento de contraseña con envio automatico de email
+                    desde el backend. A nivel frontend se desarrollo CRUD con
+                    capacidad para reordenar los componentes dentro del menu
+                    como tambien modificar el comportamiento de display de cada
+                    categoria, la distincion de productos huerfanos (sin
+                    categoria), productos no publicados, y productos publicados
+                    mediante indicador visual, entre otras caracteristicas.
+                  </p>
+                </div>
+              </a>
+              <a
+                className="px-3 pb-3"
+                href="https://github.com/Magicmilian/corchoMenuFrontEnd"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Ver en Github
+              </a>
+            </div>
+            <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-rollingnews.jpg"}
+                  className="card-img-top imgProject"
+                  alt="Imagen de RollingNews"
+                ></img>
+              </div>
+              <a
+                className="text-decoration-none projectLink text-dark"
                 href="https://rollingnews.netlify.app/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-rollingnews.jpg"}
-                    className="card-img-top imgProject"
-                    alt="Imagen de RollingNews"
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Rolling News - Pagina de noticias
@@ -57,19 +147,19 @@ const Portfolio = () => {
               </a>
             </div>
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-rollinggames.jpg"}
+                  className="card-img-top imgProject"
+                  alt="Imagen de Rolling Games"
+                ></img>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
                 href="https://rolling-games.netlify.app/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-rollinggames.jpg"}
-                    className="card-img-top imgProject"
-                    alt="Imagen de Rolling Games"
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Rolling Games - Pagina de juegos
@@ -78,7 +168,8 @@ const Portfolio = () => {
                     Proyecto realizado como presentacion del segundo modulo del
                     curso de Full Stack. Incluye el diseño y funcionalidad del
                     front end. Para la carga de contenido utiliza Local Storage
-                    como simulacion de back end. Ver detalles sobre cargar el contenido en repositorio de Github.
+                    como simulacion de back end. Ver detalles sobre cargar el
+                    contenido en repositorio de Github.
                   </p>
                 </div>
               </a>
@@ -92,49 +183,56 @@ const Portfolio = () => {
               </a>
             </div>
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-maravillosomundo.jpg"}
+                  className="card-img-top imgProject"
+                  alt="Imagen de Maravilloso Mundo"
+                ></img>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
                 href="https://maravilloso-mundo.netlify.app/index.html"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-maravillosomundo.jpg"}
-                    className="card-img-top imgProject"
-                    alt="Imagen de Maravilloso Mundo"
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Maravilloso Mundo - Blog de viajes
                   </h5>
                   <p className="card-text text-justify">
-                  Este proyecto fue realizado como presentacion final del primer modulo en el programa de desarrollo Full Stack. En este modulo pudimos comprender y aplicar el lenguaje HTML, junto a hojas de estilo CSS y utilizando el framework Bootstrap4. Este proyecto es solo un maquetado, muchas de las funcionalidades del sitio no se encuentran codificadas.
+                    Este proyecto fue realizado como presentacion final del
+                    primer modulo en el programa de desarrollo Full Stack. En
+                    este modulo pudimos comprender y aplicar el lenguaje HTML,
+                    junto a hojas de estilo CSS y utilizando el framework
+                    Bootstrap4. Este proyecto es solo un maquetado, muchas de
+                    las funcionalidades del sitio no se encuentran codificadas.
                   </p>
                 </div>
               </a>
             </div>
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-panaderiaCocu.jpg"}
+                  className="card-img-top imgProject"
+                  alt="Imagen de panaderia Cocu"
+                ></img>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
                 href="https://panaderia-bootstrap.netlify.app/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-panaderiaCocu.jpg"}
-                    className="card-img-top imgProject"
-                    alt="Imagen de panaderia Cocu"
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Panaderia Cocu - Landing Page
                   </h5>
                   <p className="card-text text-justify">
-                  Este proyecto fue realizado como practica de Bootstrap4. Se realizo el maquetado de una pagina para un comercio dedicado al rubro de Panaderia.
+                    Este proyecto fue realizado como practica de Bootstrap4. Se
+                    realizo el maquetado de una pagina para un comercio dedicado
+                    al rubro de Panaderia.
                   </p>
                 </div>
               </a>
--- src/Components/Portfolio/PortfolioEN.js
+++ src/Components/Portfolio/PortfolioEN.js
@@ -1,10 +1,11 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import "./portfolio.css";
 
 const PortfolioEN = () => {
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   const background = {
     backgroundImage: "url(/background.jpg)",
     minHeight: "calc(100vh - 80px)",
@@ -18,19 +19,111 @@ const PortfolioEN = () => {
           <p className="unicaFont portfolioTitle col-12">Projects</p>
           <div className="projectsContainer d-flex flex-wrap justify-content-around exoFont">
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <Carousel indicators={false}>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-1.jpg"}
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-2.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-3.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-4.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-5.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-6.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100 imgProjectCarousel"
+                      src={process.env.PUBLIC_URL + "project-corchomenu-7.jpg"}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
+                href="https://corcho-menu.netlify.app/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <div className="card-body">
+                  <h5 className="card-title text-center text-md-left">
+                    Corcho Menu - Digital Menu
+                  </h5>
+                  <p className="card-text text-justify">
+                    Project carried out for a Tucuman Bar in order to offer a
+                    digital option of the menu used by the commerce. It was
+                    designed with the intention to emulate the distribution of
+                    the information so that it resembles a traditional bar menu.
+                    Frontend was developed using React.js, and backend using
+                    MongoDB with Express.js. At the backend level, routes
+                    protected by JWT were applied, as well as user validation
+                    through Google reCaptcha V2 and password reset with
+                    automatic email sending from the backend. At the frontend
+                    level, CRUD was developed with features to reorder
+                    components within menu as well as modifying the display
+                    behavior of each category, the distinction of orphan
+                    products (without category), unpublished products, and
+                    published products by visual indicators, among other
+                    features.
+                  </p>
+                </div>
+              </a>
+              <a
+                className="px-3 pb-3"
+                href="https://github.com/Magicmilian/corchoMenuFrontEnd"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                See on Github
+              </a>
+            </div>
+            <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-rollingnews.jpg"}
+                  className="card-img-top imgProject"
+                  alt="..."
+                ></img>
+              </div>
+              <a
+                className="text-decoration-none projectLink text-dark"
                 href="https://rollingnews.netlify.app/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-rollingnews.jpg"}
-                    className="card-img-top imgProject"
-                    alt="..."
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Rolling News - News Page
@@ -40,7 +133,7 @@ const PortfolioEN = () => {
                     of Full Stack. It includes the design and functionality of
                     the front end, as the backend, which is responsible for
                     supplying the information that ends up populating the site.
-                    It was used also the consultation of third-party APIs.
+                    It also used the consultation of third-party APIs.
                   </p>
                 </div>
               </a>
@@ -54,19 +147,19 @@ const PortfolioEN = () => {
               </a>
             </div>
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-rollinggames.jpg"}
+                  className="card-img-top imgProject"
+                  alt="..."
+                ></img>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
                 href="https://rolling-games.netlify.app/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-rollinggames.jpg"}
-                    className="card-img-top imgProject"
-                    alt="..."
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Rolling Games - Video Games Store Page
@@ -75,7 +168,8 @@ const PortfolioEN = () => {
                     Project carried out as a presentation of the second module
                     of the Full Stack course. Includes the design and
                     functionality of the front end. For content loading it use
-                    Local Storage as back end simulation. Details about loading the content on Github.
+                    Local Storage as back end simulation. Details about loading
+                    the content on Github.
                   </p>
                 </div>
               </a>
@@ -89,21 +183,19 @@ const PortfolioEN = () => {
               </a>
             </div>
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-maravillosomundo.jpg"}
+                  className="card-img-top imgProject"
+                  alt="..."
+                ></img>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
                 href="https://maravilloso-mundo.netlify.app/index.html"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={
-                      process.env.PUBLIC_URL + "project-maravillosomundo.jpg"
-                    }
-                    className="card-img-top imgProject"
-                    alt="..."
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Maravilloso Mundo - Travels Blog
@@ -120,19 +212,19 @@ const PortfolioEN = () => {
               </a>
             </div>
             <div className="card projectElement col-12 p-0">
+              <div className="px-3 pt-3 pb-0">
+                <img
+                  src={process.env.PUBLIC_URL + "project-panaderiaCocu.jpg"}
+                  className="card-img-top imgProject"
+                  alt="..."
+                ></img>
+              </div>
               <a
-                className="text-decoration-none text-dark"
+                className="text-decoration-none projectLink text-dark"
                 href="https://panaderia-bootstrap.netlify.app/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="px-3 pt-3 pb-0">
-                  <img
-                    src={process.env.PUBLIC_URL + "project-panaderiaCocu.jpg"}
-                    className="card-img-top imgProject"
-                    alt="..."
-                  ></img>
-                </div>
                 <div className="card-body">
                   <h5 className="card-title text-center text-md-left">
                     Panaderia Cocu - Landing Page
